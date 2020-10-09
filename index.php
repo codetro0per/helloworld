0a984caf5c1197eb8e18d9205936b68f5205cf79
codetro0per <fr.moshtagh@gmail.com>
2020-10-09 20:12:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>presentation</title>
+</head>
+<body>
+<?php 
+$dev = "code Trooper" ;
+ echo "<h1>je suis un $dev </h1>";
+?>
+</body>
+</html>
